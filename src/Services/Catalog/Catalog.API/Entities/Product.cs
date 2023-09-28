6b0cef340f0b3c73be65d247682a73ca83229dc9
Merve KARABEKMEZ <78443130+mervekrbkmz@users.noreply.github.com>
2023-09-28 18:48:21 +0300

    mongodb properties

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+  public class Product //mongodb-collection
+  {
+    [BsonId] //bsonid documentin mongodbden alındığı anlamına gelir. bson kullandım.
+    [BsonRepresentation(BsonType.ObjectId)] //mongodb de id yi objectid olarak kullanmak istiyorum.
+    public string Id { get; set; }
+    [BsonElement("Name")] //attribute ekledim.
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public string Summary { get; set; }
+    public string Description { get; set; }
+    public string ImageFile { get; set; }
+    public decimal Price { get; set; }
+
+  }
+}
