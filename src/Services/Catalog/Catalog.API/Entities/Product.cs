61f80f0a66ce235544c14aca9de7dfcd774cf145
mervekrbkmz <mervekarabekmezz@gmail.com>
2023-10-11 22:24:06 +0300

    comm

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -11,7 +11,7 @@ namespace Catalog.API.Entities
     [BsonElement("Name")] //attribute ekledim.
     public string Name { get; set; }
     public string Category { get; set; }
-    public string Summary { get; set; }
+    public string Summary { get; set; } 
     public string Description { get; set; }
     public string ImageFile { get; set; }
     public decimal Price { get; set; }
