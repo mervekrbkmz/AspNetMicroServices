fa6420faebd2bd58846bab3b8846269a8d87d92e
mervekrbkmz <mervekarabekmezz@gmail.com>
2023-10-16 21:07:00 +0300

    commit

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -17,9 +17,9 @@ namespace  Catalog.API.Repositories
         {
             catalogContext=_catalogContext;
         }
-        public Task CreateProduct(Product product)
+        public async Task CreateProduct(Product product)
         {
-            throw new NotImplementedException();
+        await _catalogContext.Products.InsertOneAsync(product);
         }
 
         public Task<bool> DeleteProduct(string id)
@@ -27,13 +27,19 @@ namespace  Catalog.API.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Product>> GetProductCategory(string categoryName)
-        {
+        public  async Task<IEnumerable<Product>> GetProductCategory(string categoryName)
+        { 
+              FilterDefinition<Product> filter= Builders<Product>.Filter.Eq(p=>p.Category,categoryName);
+              return await _catalogContext
+                            .Products
+                            .Find(filter)
+                            .ToListAsync();
             throw new NotImplementedException();
         }
 
         public async Task<IEnumerable<Product>> GetProducts()
         {
+          
             return await _catalogContext 
                         .Products   
                         .Find(p=>true)
@@ -48,9 +54,13 @@ namespace  Catalog.API.Repositories
         }
    public async Task<IEnumerable<Product>> GetProductByName(string name)
         {
+             //FilterDefinition:MongoDB'de belirli bir koşula uyan belgeleri filtrelemek için kullanılır
+             //ElemMatch:eşleşip eşleşmediğini kontrol eder basit sorgularda gerek olmaz,equal kullanılmalı.
+             //eq kullanmak iyidir çünkü eq ayrıca mevcut olup olmadığını da arar.
+            FilterDefinition<Product> filter= Builders<Product>.Filter.Eq(p=>p.Name,name);
             return await _catalogContext 
                         .Products   
-                        .Find(p=>p.Name==name)
+                        .Find(filter)
                         .ToListAsync();
         }
 
@@ -58,5 +68,10 @@ namespace  Catalog.API.Repositories
         {
             throw new NotImplementedException();
         }
+
+        // public async Task<bool> UpdateProduct(Product product)
+        // {
+        //    var updateResult=await _catalogContext.Products.
+        // }
     }
 }
